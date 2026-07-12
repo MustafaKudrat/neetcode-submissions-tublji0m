32342e11d56a33edcbecde5d4b0c92836a31700e
Mustafa <45443686+MustafaKudrat@users.noreply.github.com>
2026-07-12 15:11:51 -0700

    Add: permutation-string - submission-9

---
+++ Data Structures & Algorithms/permutation-string/submission-9.py
@@ -0,0 +1,21 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        counter1 = {}
+        for c in s1:
+            counter1[c] = 1 + counter1.get(c, 0)
+        
+        need = len(counter1)
+
+        for i in range(len(s2) - len(s1) + 1):
+            counter2 = {}
+            have = 0
+            for j in range(i, i + len(s1)):
+                counter2[s2[j]] = 1 + counter2.get(s2[j], 0)
+                if counter2[s2[j]] > counter1.get(s2[j], 0):
+                    break
+                if counter2[s2[j]] == counter1.get(s2[j], 0):
+                    have += 1
+                if have == need:
+                    return True
+
+        return False
